0de20ffe2960d5170b2e237849100e0ed6d2193f
ksdyb.park <ksdyb@amazon.com>
2023-08-02 04:24:41 +0900

    update

---
+++ cdk-chatbot-llama2-kendra/test/cdk-chatbot-llama2-kendra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkChatbotLlama2Kendra from '../lib/cdk-chatbot-llama2-kendra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-chatbot-llama2-kendra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkChatbotLlama2Kendra.CdkChatbotLlama2KendraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
