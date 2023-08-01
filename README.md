905e6b0dc3d353921252df6c6a706a9330777195
ksdyb.park <ksdyb@amazon.com>
2023-08-02 04:32:11 +0900

    update

--- README.md
+++ README.md
@@ -1,2 +1,288 @@
-# Llama2-chatbot-with-kendra
-It shows a chatbot with Amazon Kendra using Llama 2.
+# Amazon JumpStart와 Amazon Kendra를 이용하여 Llama 2로 Chatbot 만들기
+
+여기서는 Llama2의 LLM 모델을 이용하여 Question/Answering을 수행하는 Chatbot을 만듧니다. Question/Answering의 정확도를 높이기 위하여 관련된 문서를 업로드하면, Kendra로 분석하여 LLM이 Question/Answering 수행시 활용할 수 있습니다. 이와같이 Kendra를 이용하면, LLM의 token 사이즈를 넘어서는 긴문장을 활용하여 Question/Answering과 같은 Task를 수행할 수 있으며 환각(hallucination) 영향을 줄일 수 있습니다. 사용자가 파일을 로드하면 CloudFont와 API Gateway를 거쳐서 [Lambda (upload)](./lambda-upload/index.js)가 S3에 파일을 저장합니다. 저장이 완료되면 해당 Object의 bucket과 key 정보를 kendra에 전달합니다. 이후 사용자가 메시지를 질문을 하면, kendra가 가장 관련이 있는 문장을 LLM에 전달하고, 이를 이용하여 좀 더 정확한 답변을 얻을 수 있습니다. call log는 DynamoDB에 저장하여 확인할 수 있습니다.
+
+전체적인 Architecture는 아래와 같습니다. 사용자가 파일을 업로드하면 Amazon S3에 저장된 후, kendra에 전달되어 symantic search에 활용되어집니다. 이후 사용자가 텍스트로 질문을 하면, CloudFront - API Gateway를 지나 [Lambda (chat)](./lambda-chat/lambda_function.py)에 텍스트가 전달됩니다. 이제 kendra를 통해 검색을 수행하여, 미리 입력한 문서중에서 가까운 문장을 얻습니다. 이후 Llama 2 LLM을 이용하여 답변을 얻습니다. 답변은 DynamoDB에 call log의 형태로 저장되어 추후 각종 통계정보나 결과를 분석하는데에 이용될 수 있습니다. Llama 2 LLM은 SageMaker Endpoint를 이용하여 LangChain 형식의 API를 통해 구현하였고, Chatbot을 제공하는 인프라는 AWS CDK를 통해 배포합니다. 
+
+<img src="https://github.com/kyopark2014/question-answering-chatbot-with-kendra/assets/52392004/e27d5831-91cc-44ce-ba6a-ff837b9f33c7" width="800">
+
+문서파일을 업로드하여 Kendra에 저장하는 과정은 아래와 같습니다.
+
+1) 사용자가 파일 업로드를 요청합니다. 이때 사용하는 Upload API는 [lambda (upload)](.lambda-upload/index.js)는 S3 presigned url을 생성하여 전달합니다.
+2) 이후 presigned url로 문서를 업로드 하면 S3에 Object로 저장됩니다.
+3) Chat API에서 request type을 'document'로 지정하면 [lambda (chat)](./lambda-chat/index.js)은 S3에서 object를 로드하여 텍스트를 추출합니다.
+4) 추출한 텍스트를 Kendra로 전달합니다.
+
+채팅 창에서 텍스트 입력(Prompt)를 통해 Kendra로 RAG를 활용하는 과정은 아래와 같습니다.
+1) 사용자가 채팅창에서 질문(Question)을 입력합니다.
+2) 이것은 Chat API를 이용하여 [lambda (chat)](./lambda-chat/index.js)에 전달됩니다.
+3) lambda(chat)은 Kendra에 질문과 관련된 문장이 있는지 확인합니다.
+4) Kendra로 부터 얻은 관련된 문장들로 prompt template를 생성하여 대용량 언어 모델(LLM) Endpoint로 질문을 전달합니다. 이후 답변을 받으면 사용자에게 결과를 전달합니다.
+5) 결과는 DyanmoDB에 저장되어 이후 데이터 분석등의 목적을 위해 활용됩니다.
+
+
+## 주요 구성
+
+### IAM Role
+
+IAM Role에서 아래와 같이 kendra에 대한 Permission을 추가해야 합니다.
+
+```java
+{
+    "Effect": "Allow",
+    "Action": [
+        "kendra:*"
+    ],
+    "Resource": "arn:aws:kendra:[your-region]:[your-account-id]:index/[index-id]"
+}]
+```
+
+이를 [cdk-chatbot-with-kendra-stack.ts](./cdk-chatbot-with-kendra/lib/cdk-chatbot-with-kendra-stack.ts)에서는 아래와 구현할 수 있습니다.
+
+```java
+const region = process.env.CDK_DEFAULT_REGION;
+const accountId = process.env.CDK_DEFAULT_ACCOUNT;
+const kendraResourceArn = `arn:aws:kendra:${region}:${accountId}:index/${kendraIndex}`
+if (debug) {
+    new cdk.CfnOutput(this, `resource-arn-of-kendra-for-${projectName}`, {
+        value: kendraResourceArn,
+        description: 'The arn of resource',
+    });
+}
+const kendraPolicy = new iam.PolicyStatement({
+    resources: [kendraResourceArn],
+    actions: ['kendra:*'],
+});
+
+roleLambda.attachInlinePolicy( // add kendra policy
+    new iam.Policy(this, `kendra-policy-for-${projectName}`, {
+        statements: [kendraPolicy],
+    }),
+);  
+```
+
+Kendra를 위한 trust policy는 아래와 같이 설정합니다.
+
+```java
+{
+   "Version":"2012-10-17",
+   "Statement":[
+      {
+         "Effect":"Allow",
+         "Principal":{
+            "Service":"kendra.amazonaws.com"
+         },
+         "Action":"sts:AssumeRole"
+      }
+   ]
+}
+```
+
+따라서, [cdk-chatbot-with-kendra-stack.ts](./cdk-chatbot-with-kendra/lib/cdk-chatbot-with-kendra-stack.ts)와 같이 "kendra.amazonaws.com"을 추가합니다.
+
+```java
+const roleLambda = new iam.Role(this, `role-lambda-chat-for-${projectName}`, {
+    roleName: `role-lambda-chat-for-${projectName}`,
+    assumedBy: new iam.CompositePrincipal(
+        new iam.ServicePrincipal("lambda.amazonaws.com"),
+        new iam.ServicePrincipal("kendra.amazonaws.com")
+    )
+});
+```
+
+[Troubleshooting Amazon Kendra Identity and Access](https://docs.aws.amazon.com/kendra/latest/dg/security_iam_troubleshoot.html)와 같아 Kendra는 "iam:PassRole"을 포함하여야 합니다. 
+
+```java
+{
+    "Action": [
+        "iam:PassRole"
+    ],
+    "Resource": [
+        "arn:aws:iam::[account-id]:role/role-lambda-chat-for-chatbot-with-kendra",
+    ],
+    "Effect": "Allow"
+}
+```
+
+이를 [cdk-chatbot-with-kendra-stack.ts](./cdk-chatbot-with-kendra/lib/cdk-chatbot-with-kendra-stack.ts)에서는 아래와 같이 구현할 수 있습니다.
+
+```java
+const passRoleResourceArn = roleLambda.roleArn;
+const passRolePolicy = new iam.PolicyStatement({
+    resources: [passRoleResourceArn],
+    actions: ['iam:PassRole'],
+});
+roleLambda.attachInlinePolicy(
+    new iam.Policy(this, `pass-role-of-kendra-for-${projectName}`, {
+        statements: [passRolePolicy],
+    }),
+);
+```  
+
+### LangChain으로 연결하기
+
+LangChain을 이용해서 Llama 2에 연결하는 경우에 아래와 같이 endpoint_kwargs에 CustomAttributes를 추가합니다. 
+
+```python
+endpoint_name = os.environ.get('endpoint')
+
+class ContentHandler(LLMContentHandler):
+    content_type = "application/json"
+    accepts = "application/json"
+
+    def transform_input(self, prompt: str, model_kwargs: dict) -> bytes:
+        input_str = json.dumps({
+            "inputs" : 
+            [
+                [
+                    {
+                        "role" : "system",
+                        "content" : "You are a kind robot."
+                    },
+                    {
+                        "role" : "user", 
+                        "content" : prompt
+                    }
+                ]
+            ],
+            "parameters" : {**model_kwargs}})
+        return input_str.encode('utf-8')
+      
+    def transform_output(self, output: bytes) -> str:
+        response_json = json.loads(output.read().decode("utf-8"))
+        return response_json[0]["generation"]["content"]
+
+content_handler = ContentHandler()
+aws_region = boto3.Session().region_name
+client = boto3.client("sagemaker-runtime")
+parameters = {
+    "max_new_tokens": 256, 
+    "top_p": 0.9, 
+    "temperature": 0.6
+} 
+
+llm = SagemakerEndpoint(
+    endpoint_name = endpoint_name, 
+    region_name = aws_region, 
+    model_kwargs = parameters,
+    endpoint_kwargs={"CustomAttributes": "accept_eula=true"},
+    content_handler = content_handler
+)
+```
+
+
+### Kendra
+
+### 문서 등록
+
+S3에 저장된 문서를 kendra로 전달하기 위하여, 아래와 같이 문서에 대한 S3 정보를 kendra의 [batch_put_document()](https://docs.aws.amazon.com/kendra/latest/APIReference/API_BatchPutDocument.html)을 이용하여 전달합니다. 
+
+```python
+documentInfo = {
+    "S3Path": {
+        "Bucket": s3_bucket,
+        "Key": s3_prefix+'/'+s3_file_name
+    },
+    "Title": "Document from client",
+    "Id": requestId
+}
+
+documents = [
+    documentInfo
+]
+
+kendra = boto3.client("kendra")
+kendra.batch_put_document(
+    Documents = documents,
+    IndexId = kendraIndex,
+    RoleArn = roleArn
+)
+```
+
+업로드한 문서 파일에 대한 정보를 사용자에게 보여주기 위하여 아래와 같이 요약(Summerization)을 수행합니다.
+
+```python
+file_type = object[object.rfind('.') + 1: len(object)]
+print('file_type: ', file_type)
+
+docs = load_document(file_type, object)
+prompt_template = """Write a concise summary of the following:
+
+{ text }
+                
+CONCISE SUMMARY """
+
+PROMPT = PromptTemplate(template = prompt_template, input_variables = ["text"])
+chain = load_summarize_chain(llm, chain_type = "stuff", prompt = PROMPT)
+summary = chain.run(docs)
+print('summary: ', summary)
+
+msg = summary
+```
+
+
+### Question/Answering
+
+사용자가 채팅창에서 메시지를 입력할때 발생한 메시지는 아래처럼 query로 전달되고, [Kendra Retriever](https://python.langchain.com/docs/integrations/retrievers/amazon_kendra_retriever)를 이용하여 get_relevant_documents()로 관련된 문장들을 kendra로부터 가져옵니다. 이때 가져온 문장이 없다면 Llama 2의 llm()을 이용하여 결과를 얻고, kendra에 관련된 데이터가 있다면 아래와 같이 template을 이용하여 [RetrievalQA](https://python.langchain.com/docs/modules/chains/popular/vector_db_qa)로 query에 대한 응답을 구하여 결과로 전달합니다.
+
+```python
+relevant_documents = retriever.get_relevant_documents(query)
+
+if (len(relevant_documents) == 0):
+    return llm(query)
+else:
+    prompt_template = """Human: Use the following pieces of context to provide a concise answer to the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+    { context }
+
+    Question: { question }
+    Assistant: """
+    PROMPT = PromptTemplate(
+        template = prompt_template, input_variables = ["context", "question"]
+    )
+
+    qa = RetrievalQA.from_chain_type(
+        llm = llm,
+        chain_type = "stuff",
+        retriever = retriever,
+        return_source_documents = True,
+        chain_type_kwargs = { "prompt": PROMPT }
+    )
+    result = qa({ "query": query })
+
+    return result['result']
+```
+
+## 실습하기
+
+### CDK를 이용한 인프라 설치
+[인프라 설치](./deployment.md)에 따라 CDK로 인프라 설치를 진행합니다. [CDK 구현 코드](./cdk-chatbot-with-kendra/README.md)에서는 Typescript로 인프라를 정의하는 방법에 대해 상세히 설명하고 있습니다.
+
+
+## 실행결과
+
+파일을 올리면 먼저 파일을 S3에 올리고, 이후로 kendra에 등록합니다. 업로드 한 파일의 내용을 확인하기 위하여 아래와 같이 요약(Summeraztion)을 수행합니다.
+
+![image](https://github.com/kyopark2014/question-answering-chatbot-with-kendra/assets/52392004/74768245-6738-4a14-b942-cb6a9f39d252)
+
+이후 아래와 같이 문서 내용에 대해 질문을 하면 답변을 얻을 수 있습니다.
+
+![image](https://github.com/kyopark2014/question-answering-chatbot-with-kendra/assets/52392004/877d04a7-8190-43b5-a9fc-eab9e00ab990)
+
+
+#### Chatbot 동작 시험시 주의할점
+
+일반적인 chatbot들은 지속적인 세션을 유지 관리하기 위해서는 websocket 등을 사용하지만, 여기서 사용한 Chatbot은 API를 테스트하기 위하여 RESTful API를 사용하고 있습니다. 따라서, LLM에서 응답이 일정시간(30초)이상 지연되는 경우에 브라우저에서 답변을 볼 수 없습니다. 따라서 긴 응답시간이 필요한 경우에 CloudWatch에서 [lambda-chat](./lambda-chat/lambda_function.py)의 로그를 확인하거나, DynamoDB에 저장된 call log를 확인합니다.
+
+
+## Reference 
+
+[Kendra - LangChain](https://python.langchain.com/docs/integrations/retrievers/amazon_kendra_retriever)
+
+[kendra_chat_anthropic.py](https://github.com/aws-samples/amazon-kendra-langchain-extensions/blob/main/kendra_retriever_samples/kendra_chat_anthropic.py)
+
+[IAM access roles for Amazon Kendra](https://docs.aws.amazon.com/kendra/latest/dg/iam-roles.html)
+
+[Adding documents with the BatchPutDocument API](https://docs.aws.amazon.com/kendra/latest/dg/in-adding-binary-doc.html)
+
+[class CfnIndex (construct)](https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_kendra.CfnIndex.html)
--- cdk-chatbot-llama2-kendra/lib/cdk-chatbot-llama2-kendra-stack.ts
+++ cdk-chatbot-llama2-kendra/lib/cdk-chatbot-llama2-kendra-stack.ts
@@ -1,16 +1,334 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as path from "path";
+import * as logs from "aws-cdk-lib/aws-logs"
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudFront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as apiGateway from 'aws-cdk-lib/aws-apigateway';
+import * as s3Deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as kendra from 'aws-cdk-lib/aws-kendra';
+
+const debug = false;
+const stage = 'dev';
+const s3_prefix = 'docs';
+const bedrock_region = "us-west-2";
+const endpoint_url = "https://prod.us-west-2.frontend.bedrock.aws.dev";
+const model_id = "amazon.titan-tg1-large"; // amazon.titan-e1t-medium, anthropic.claude-v1
+const projectName = "llama2-with-kendra";
+const bucketName = `storage-for-${projectName}`;
 
 export class CdkChatbotLlama2KendraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // s3 
+    const s3Bucket = new s3.Bucket(this, `storage-${projectName}`,{
+      bucketName: bucketName,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      publicReadAccess: false,
+      versioned: false,
+      cors: [
+        {
+          allowedHeaders: ['*'],
+          allowedMethods: [
+            s3.HttpMethods.POST,
+            s3.HttpMethods.PUT,
+          ],
+          allowedOrigins: ['*'],
+        },
+      ],
+    });
+    if(debug) {
+      new cdk.CfnOutput(this, 'bucketName', {
+        value: s3Bucket.bucketName,
+        description: 'The nmae of bucket',
+      });
+      new cdk.CfnOutput(this, 's3Arn', {
+        value: s3Bucket.bucketArn,
+        description: 'The arn of s3',
+      });
+      new cdk.CfnOutput(this, 's3Path', {
+        value: 's3://'+s3Bucket.bucketName,
+        description: 'The path of s3',
+      });
+    }
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkChatbotLlama2KendraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // DynamoDB for call log
+    const callLogTableName = `db-call-log-for-${projectName}`;
+    const callLogDataTable = new dynamodb.Table(this, `db-call-log-for-${projectName}`, {
+      tableName: callLogTableName,
+      partitionKey: { name: 'user-id', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'request-id', type: dynamodb.AttributeType.STRING }, 
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+    const callLogIndexName = `index-type-for-${projectName}`;
+    callLogDataTable.addGlobalSecondaryIndex({ // GSI
+      indexName: callLogIndexName,
+      partitionKey: { name: 'type', type: dynamodb.AttributeType.STRING },
+    });
+
+    // DynamoDB for configuration
+    const configTableName = `db-configuration-for-${projectName}`;
+    const configDataTable = new dynamodb.Table(this, `dynamodb-configuration-for-${projectName}`, {
+      tableName: configTableName,
+      partitionKey: { name: 'user-id', type: dynamodb.AttributeType.STRING },      
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // copy web application files into s3 bucket
+    new s3Deploy.BucketDeployment(this, `upload-HTML-for-${projectName}`, {
+      sources: [s3Deploy.Source.asset("../html")],
+      destinationBucket: s3Bucket,
+    });
+    
+    // cloudfront
+    const distribution = new cloudFront.Distribution(this, `cloudfront-for-${projectName}`, {
+      defaultBehavior: {
+        origin: new origins.S3Origin(s3Bucket),
+        allowedMethods: cloudFront.AllowedMethods.ALLOW_ALL,
+        cachePolicy: cloudFront.CachePolicy.CACHING_DISABLED,
+        viewerProtocolPolicy: cloudFront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      priceClass: cloudFront.PriceClass.PRICE_CLASS_200,  
+    });
+    new cdk.CfnOutput(this, `distributionDomainName-for-${projectName}`, {
+      value: distribution.domainName,
+      description: 'The domain name of the Distribution',
+    });
+
+    // Kendra    
+    const roleKendra = new iam.Role(this, `role-kendra-for-${projectName}`, {
+      roleName: `role-kendra-for-${projectName}`,
+      assumedBy: new iam.CompositePrincipal(
+        new iam.ServicePrincipal("kendra.amazonaws.com")
+      )
+    });
+    const cfnIndex = new kendra.CfnIndex(this, 'MyCfnIndex', {
+      edition: 'DEVELOPER_EDITION',  // ENTERPRISE_EDITION
+      name: `reg-kendra-${projectName}`,
+      roleArn: roleKendra.roleArn,
+    });     
+    new cdk.CfnOutput(this, `index-of-kendra-for-${projectName}`, {
+      value: cfnIndex.attrId,
+      description: 'The index of kendra',
+    }); 
+
+    const region = process.env.CDK_DEFAULT_REGION;
+    const accountId = process.env.CDK_DEFAULT_ACCOUNT;
+    const kendraResourceArn = `arn:aws:kendra:${region}:${accountId}:index/${cfnIndex.attrId}`
+    if(debug) {
+      new cdk.CfnOutput(this, `resource-arn-of-kendra-for-${projectName}`, {
+        value: kendraResourceArn,
+        description: 'The arn of resource',
+      }); 
+    }           
+    const kendraPolicy = new iam.PolicyStatement({  
+      resources: [kendraResourceArn],      
+      actions: ['kendra:*'],
+    });      
+    roleKendra.attachInlinePolicy( // add kendra policy
+      new iam.Policy(this, `kendra-inline-policy-for-${projectName}`, {
+        statements: [kendraPolicy],
+      }),
+    );  
+          
+    const roleLambda = new iam.Role(this, `role-lambda-chat-for-${projectName}`, {
+      roleName: `role-lambda-chat-for-${projectName}`,
+      assumedBy: new iam.CompositePrincipal(
+        new iam.ServicePrincipal("lambda.amazonaws.com"),
+        new iam.ServicePrincipal("bedrock.amazonaws.com"),
+        new iam.ServicePrincipal("kendra.amazonaws.com")
+      )
+    });
+    roleLambda.addManagedPolicy({
+      managedPolicyArn: 'arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole',
+    });
+    const BedrockPolicy = new iam.PolicyStatement({ 
+      resources: ['*'],
+      actions: ['bedrock:*'],
+    });        
+    roleLambda.attachInlinePolicy( // add bedrock policy
+      new iam.Policy(this, `lambda-inline-policy-for-bedrock-in-${projectName}`, {
+        statements: [BedrockPolicy],
+      }),
+    );         
+    roleLambda.attachInlinePolicy( // add kendra policy
+      new iam.Policy(this, `lambda-inline-policy-for-kendra-in-${projectName}`, {
+        statements: [kendraPolicy],
+      }),
+    );  
+   
+    const passRoleResourceArn = roleLambda.roleArn;
+    const passRolePolicy = new iam.PolicyStatement({  
+      resources: [passRoleResourceArn],      
+      actions: ['iam:PassRole'],
+    });
+    roleLambda.attachInlinePolicy( // add pass role policy
+      new iam.Policy(this, `pass-role-of-kendra-for-${projectName}`, {
+        statements: [passRolePolicy],
+      }), 
+    );  
+    
+    new cdk.CfnOutput(this, `passRole-resource-arn-of-kendra-for-${projectName}`, {
+      value: passRoleResourceArn,
+      description: 'The arn of pass role',
+    }); 
+
+    // Lambda for chat using langchain (container)
+    const lambdaChatApi = new lambda.DockerImageFunction(this, `lambda-chat-for-${projectName}`, {
+      description: 'lambda for chat api',
+      functionName: `lambda-chat-api-for-${projectName}`,
+      code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../../lambda-chat')),
+      timeout: cdk.Duration.seconds(60),
+      memorySize: 4096,
+      role: roleLambda,
+      environment: {
+        bedrock_region: bedrock_region,
+        endpoint_url: endpoint_url,
+        model_id: model_id,
+        s3_bucket: s3Bucket.bucketName,
+        s3_prefix: s3_prefix,
+        callLogTableName: callLogTableName,
+        configTableName: configTableName,
+        kendraIndex: cfnIndex.attrId,
+        roleArn: roleLambda.roleArn
+      }
+    });     
+    lambdaChatApi.grantInvoke(new iam.ServicePrincipal('apigateway.amazonaws.com'));  
+    s3Bucket.grantRead(lambdaChatApi); // permission for s3
+    callLogDataTable.grantReadWriteData(lambdaChatApi); // permission for dynamo
+    configDataTable.grantReadWriteData(lambdaChatApi); // permission for dynamo
+
+    // role
+    const role = new iam.Role(this, `api-role-for-${projectName}`, {
+      roleName: `api-role-for-${projectName}`,
+      assumedBy: new iam.ServicePrincipal("apigateway.amazonaws.com")
+    });
+    role.addToPolicy(new iam.PolicyStatement({
+      resources: ['*'],
+      actions: ['lambda:InvokeFunction']
+    }));
+    role.addManagedPolicy({
+      managedPolicyArn: 'arn:aws:iam::aws:policy/AWSLambdaExecute',
+    }); 
+
+    // API Gateway
+    const api = new apiGateway.RestApi(this, `api-chatbot-for-${projectName}`, {
+      description: 'API Gateway for chatbot',
+      endpointTypes: [apiGateway.EndpointType.REGIONAL],
+      binaryMediaTypes: ['application/pdf', 'text/plain', 'text/csv'], 
+      deployOptions: {
+        stageName: stage,
+
+        // logging for debug
+        loggingLevel: apiGateway.MethodLoggingLevel.INFO, 
+        dataTraceEnabled: true,
+      },
+    });  
+
+    // POST method
+    const chat = api.root.addResource('chat');
+    chat.addMethod('POST', new apiGateway.LambdaIntegration(lambdaChatApi, {
+      passthroughBehavior: apiGateway.PassthroughBehavior.WHEN_NO_TEMPLATES,
+      credentialsRole: role,
+      integrationResponses: [{
+        statusCode: '200',
+      }], 
+      proxy:false, 
+    }), {
+      methodResponses: [   // API Gateway sends to the client that called a method.
+        {
+          statusCode: '200',
+          responseModels: {
+            'application/json': apiGateway.Model.EMPTY_MODEL,
+          }, 
+        }
+      ]
+    }); 
+
+    if(debug) {
+      new cdk.CfnOutput(this, `apiUrl-chat-for-${projectName}`, {
+        value: api.url,
+        description: 'The url of API Gateway',
+      }); 
+      new cdk.CfnOutput(this, `curlUrl-chat-for-${projectName}`, {
+        value: "curl -X POST "+api.url+'chat -H "Content-Type: application/json" -d \'{"text":"who are u?"}\'',
+        description: 'Curl commend of API Gateway',
+      }); 
+    }
+
+    // cloudfront setting for api gateway of stable diffusion
+    distribution.addBehavior("/chat", new origins.RestApiOrigin(api), {
+      cachePolicy: cloudFront.CachePolicy.CACHING_DISABLED,
+      allowedMethods: cloudFront.AllowedMethods.ALLOW_ALL,  
+      viewerProtocolPolicy: cloudFront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    });    
+   
+    new cdk.CfnOutput(this, `WebUrl-for-${projectName}`, {
+      value: 'https://'+distribution.domainName+'/chat.html',      
+      description: 'The web url of request for chat',
+    });
+
+    new cdk.CfnOutput(this, `UpdateCommend-for-${projectName}`, {
+      value: 'aws s3 cp ../html/chat.js '+'s3://'+s3Bucket.bucketName,
+      description: 'The url of web file upload',
+    });
+
+    // Lambda - Upload
+    const lambdaUpload = new lambda.Function(this, `lambda-upload-for-${projectName}`, {
+      runtime: lambda.Runtime.NODEJS_16_X, 
+      functionName: `lambda-upload-for-${projectName}`,
+      code: lambda.Code.fromAsset("../lambda-upload"), 
+      handler: "index.handler", 
+      timeout: cdk.Duration.seconds(10),
+      logRetention: logs.RetentionDays.ONE_DAY,
+      environment: {
+        bucketName: s3Bucket.bucketName,
+        s3_prefix:  s3_prefix
+      }      
+    });
+    s3Bucket.grantReadWrite(lambdaUpload);
+    
+    // POST method - upload
+    const resourceName = "upload";
+    const upload = api.root.addResource(resourceName);
+    upload.addMethod('POST', new apiGateway.LambdaIntegration(lambdaUpload, {
+      passthroughBehavior: apiGateway.PassthroughBehavior.WHEN_NO_TEMPLATES,
+      credentialsRole: role,
+      integrationResponses: [{
+        statusCode: '200',
+      }], 
+      proxy:false, 
+    }), {
+      methodResponses: [  
+        {
+          statusCode: '200',
+          responseModels: {
+            'application/json': apiGateway.Model.EMPTY_MODEL,
+          }, 
+        }
+      ]
+    }); 
+    if(debug) {
+      new cdk.CfnOutput(this, `ApiGatewayUrl-for-${projectName}`, {
+        value: api.url+'upload',
+        description: 'The url of API Gateway',
+      }); 
+    }
+
+    // cloudfront setting for api gateway    
+    distribution.addBehavior("/upload", new origins.RestApiOrigin(api), {
+      cachePolicy: cloudFront.CachePolicy.CACHING_DISABLED,
+      allowedMethods: cloudFront.AllowedMethods.ALLOW_ALL,  
+      viewerProtocolPolicy: cloudFront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    });    
   }
 }
