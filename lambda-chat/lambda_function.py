f307a40d742b789be790b6d9516051ee9e028861
ksdyb.park <ksdyb@amazon.com>
2023-08-23 16:26:42 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -136,7 +136,7 @@ def load_document(file_type, s3_file_name):
     ]
     return docs
               
-def get_answer_using_template(query):    
+def get_answer_using_template_backup(query):    
     relevant_documents = retriever.get_relevant_documents(query)
     print('length of relevant_documents: ', len(relevant_documents))
     print('relevant_documents: ', relevant_documents)    
@@ -173,7 +173,32 @@ def get_answer_using_template(query):
         print(source_documents)
 
         return result['result']
+
+def get_answer_using_template(query):    
+    prompt_template = """Human: Use the following pieces of context to provide a concise answer to the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+    {context}
+
+    Question: {question}
+    Assistant:"""
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=retriever,
+        return_source_documents=True,
+        chain_type_kwargs={"prompt": PROMPT}
+    )
+    result = qa({"query": query})
         
+    source_documents = result['source_documents']
+    print(source_documents)
+
+    return result['result']
+
 def lambda_handler(event, context):
     print(event)
     userId  = event['user-id']
