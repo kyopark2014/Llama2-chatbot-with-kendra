11fe83711c80572b60da1417f9a88deefa86ba17
ksdyb.park <ksdyb@amazon.com>
2023-08-23 16:54:54 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -18,6 +18,7 @@ from langchain.document_loaders import CSVLoader
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain.retrievers import AmazonKendraRetriever
+from langchain.chains import ConversationalRetrievalChain
 
 s3 = boto3.client('s3')
 s3_bucket = os.environ.get('s3_bucket') # bucket name
@@ -175,6 +176,38 @@ def get_answer_using_template_backup(query):
         return result['result']
 
 def get_answer_using_template(query):    
+    prompt_template = """
+        The following is a friendly conversation between a human and an AI. 
+        The AI is talkative and provides lots of specific details from its context.
+        If the AI does not know the answer to a question, it truthfully says it 
+        does not know.
+        {context}
+        Instruction: Based on the above documents, provide a detailed answer for, {question} Answer "don't know" 
+        if not present in the document. 
+        Solution:"""
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"],
+    )
+
+    condense_qa_template = """
+        Given the following conversation and a follow up question, rephrase the follow up question 
+        to be a standalone question.
+
+        Chat History:
+        {chat_history}
+        Follow Up Input: {question}
+        Standalone question:"""
+    standalone_question_prompt = PromptTemplate.from_template(condense_qa_template)
+
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=llm, 
+        retriever=retriever, 
+        condense_question_prompt=standalone_question_prompt, 
+        return_source_documents=True, 
+        combine_docs_chain_kwargs={"prompt":PROMPT},
+    )
+
+
     prompt_template = """Human: Use the following pieces of context to provide a concise answer to the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
     {context}
