327b3c3feeb2d1de6b12f80c6954b2456be8da58
ksdyb.park <ksdyb@amazon.com>
2023-08-23 22:36:04 +0900

    update

--- lambda-chat/lambda_function.py
+++ lambda-chat/lambda_function.py
@@ -175,71 +175,33 @@ def get_answer_using_template_backup(query):
 
         return result['result']
 
+def get_kendra_results_RetrieveAPI(question, index_id):
+    kendra = boto3.client('kendra',region_name="us-east-1")
+    try:
+        page_size = 2
+        page_number = 1
+        result = kendra.retrieve(
+                IndexId = index_id,
+                QueryText = question,
+                PageSize = page_size,
+                PageNumber = page_number)
+    except Exception as err:
+        print(err)
+        return "RELAVENT PASSAGES NOT FOUND"
+    
+    context =""
+    for retrieve_result in result["ResultItems"]:
+        context =context +'['
+        context = context + "Title: " + str(retrieve_result["DocumentTitle"] + ", URI: " + str(retrieve_result["DocumentURI"]) +", Passage content: " + str(retrieve_result["Content"]))
+        context =context + '] '
+    return context
+
 def get_answer_using_template(query):    
-    prompt_template = """
-        The following is a friendly conversation between a human and an AI. 
-        The AI is talkative and provides lots of specific details from its context.
-        If the AI does not know the answer to a question, it truthfully says it 
-        does not know.
-        {context}
-        Instruction: Based on the above documents, provide a detailed answer for, {question} Answer "don't know" 
-        if not present in the document. 
-        Solution:"""
-    PROMPT = PromptTemplate(
-        template=prompt_template, input_variables=["context", "question"],
-    )
 
-    condense_qa_template = """
-        Given the following conversation and a follow up question, rephrase the follow up question 
-        to be a standalone question.
-
-        Chat History:
-        {chat_history}
-        Follow Up Input: {question}
-        Standalone question:"""
-    standalone_question_prompt = PromptTemplate.from_template(condense_qa_template)
-
-    qa = ConversationalRetrievalChain.from_llm(
-        llm=llm, 
-        retriever=retriever, 
-        condense_question_prompt=standalone_question_prompt, 
-        return_source_documents=True, 
-        combine_docs_chain_kwargs={"prompt":PROMPT},
-    )
-
-    #result = qa({"query": query})
-
-    result = qa({"question": query, "chat_history": []})
-        
-    source_documents = result['source_documents']
-    print(source_documents)
-
-
-    prompt_template = """Human: Use the following pieces of context to provide a concise answer to the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
-
-    {context}
-
-    Question: {question}
-    Assistant:"""
-    PROMPT = PromptTemplate(
-        template=prompt_template, input_variables=["context", "question"]
-    )
-
-    """
-    qa = RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type="stuff",
-        retriever=retriever,
-        return_source_documents=True,
-        chain_type_kwargs={"prompt": PROMPT}
-    )
-    result = qa({"query": query})
-        
-    source_documents = result['source_documents']
-    print(source_documents)
-    """
-
-    return result['result']
+    context = get_kendra_results_RetrieveAPI(query, kendraIndex)
+    print('context: ', context)
+    
+    return context
 
 def lambda_handler(event, context):
     print(event)
